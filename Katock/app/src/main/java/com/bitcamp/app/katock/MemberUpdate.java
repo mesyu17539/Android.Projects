416c662b20aa2dd0b60be6744828a878eed76896
sangwoo <mesyu17539@gmail.com>
2018-02-09 10:37:51 +0900

    오전 10:37 2018-02-09

--- Katock/app/src/main/java/com/bitcamp/app/katock/MemberUpdate.java
+++ Katock/app/src/main/java/com/bitcamp/app/katock/MemberUpdate.java
@@ -1,6 +1,8 @@
 package com.bitcamp.app.katock;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -15,7 +17,7 @@ public class MemberUpdate extends AppCompatActivity {
         setContentView(R.layout.memberupdate);
 
         final Intent intent=this.getIntent();
-        String arr[]=intent.toString().split(",");
+        String arr[]=intent.getStringExtra(Intro.TABLE_MEMBER).split(",");
 
         final ImageView photo=findViewById(R.id.profilephoto);
         final EditText nname=findViewById(R.id.after_name);
@@ -25,8 +27,21 @@ public class MemberUpdate extends AppCompatActivity {
         final TextView name=findViewById(R.id.name);
         name.setText(arr[2]+" 프로필 수정");
         nname.setText(arr[2]);
+        nemail.setText(arr[3]);
+        nphone.setText(arr[4]);
         naddress.setText(arr[6]);
-
+        BitmapFactory.Options options= new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        Bitmap orgImage = BitmapFactory.decodeResource(getResources(),
+                this.getResources()
+                        .getIdentifier(
+                                this.getPackageName()+":drawable/"
+                                        +arr[5],
+                                null, null),
+                options
+        );
+        Bitmap resize = orgImage.createScaledBitmap(orgImage, 100, 100, true);
+        photo.setImageBitmap(resize);
 
 
     }
